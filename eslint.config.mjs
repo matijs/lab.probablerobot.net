73aca6a760edc21f95b70c508fb598d94f4d1be4
matijs <matijs@gmail.com>
2024-08-31 10:59:05 +0200

    chore: improve ESLint configuration
    - Add comments to clarify
    - Add linterOptions to disallow inline configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,13 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
+  // globally disable inline configuration, this also removes the need to use
+  // `reportUnusedDisableDirectives` because these don't work anyway
+  {
+    linterOptions: {
+      noInlineConfig: true,
+    },
+  },
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   {
@@ -11,6 +18,7 @@ export default tseslint.config(
     },
   },
   eslintConfigPrettier,
+  // `ignores` on its own is equivalent to a global ignore
   {
     ignores: ['**/.netlify/'],
   },
